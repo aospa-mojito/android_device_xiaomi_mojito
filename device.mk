393d024dc94fa45031e5b6b96d1a2bcf74d95857
Fiqri Ardyansyah <fiqri0927936@gmail.com>
2022-11-30 18:18:04 +0700

    mojito: Switch to common QTI perf component
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- device.mk
+++ device.mk
@@ -338,10 +338,6 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
-
 # Platform
 TARGET_BOARD_PLATFORM := sm6150
 
